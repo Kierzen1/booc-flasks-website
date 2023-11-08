1d4db09b2083771821190d5e9f6322f42206c7ab
Kierzen1 <92258013+Kierzen1@users.noreply.github.com>
2023-11-08 07:58:56 +0000

    edit of login and dashboard without functionalities

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#@app.route("/dashboard")
+#def dashboard():
+#  return render_template("dashboard.html")
 
-@app.route("/")
+
+@app.route("/login")
 def hello_world():
   return render_template("dashboard.html")
 
